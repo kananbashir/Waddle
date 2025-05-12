df2e58a39bcecd36866c4998d9b54117fddc27c0
Keenan Bashir <besirov.k@gmail.com>
2025-05-12 20:56:33 +0400

    feat(theme): adding shapes

---
+++ theme/src/main/java/com/waddleup/theme/Shape.kt
@@ -0,0 +1,19 @@
+package com.waddleup.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created on 5/12/2025
+ * @author Kanan Bashir
+ */
+
+val LocalAppShapes = staticCompositionLocalOf {
+    Shapes(
+        small = RoundedCornerShape(size = 16.dp),
+        medium = RoundedCornerShape(size = 32.dp),
+        large = RoundedCornerShape(size = 48.dp)
+    )
+}
